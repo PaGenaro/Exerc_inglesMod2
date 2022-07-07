b112eeb8c63a465f5d694ae1083f612a3073644b
PaGenaro <80373174+PaGenaro@users.noreply.github.com>
2022-07-06 22:06:26 -0300

    "Exercicio ingles"

---
+++ javascript.js
@@ -0,0 +1,32 @@
+//Crie 3 variáveis, cada uma com um array:
+//Dentro de cada array, insira 3 (TRÊS) Objetos:
+
+let Comedy=[{
+     title:"Norbit - Uma Comédia de Peso",
+     director:"Brian Robbins",
+     year:2007
+
+    }
+]
+
+console.log(Comedy)
+
+let Action=[{
+    title:"Venom: Tempo de Carnificina",
+    director:"Andy Serkis",
+    year: 2021
+
+   }
+]
+
+console.log(Action)
+
+let Fantasy =[{
+    title:"Raya e O Último Dragão",
+    director:"Carlos López Estrada, Don Hall",
+    year:  2021
+
+   }
+]
+
+console.log(Fantasy)
